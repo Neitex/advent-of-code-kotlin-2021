ddd48febb77e7d14209029c27047c3955528faf0
Pavel Matusevich <neitex@protonmail.com>
2021-12-01 12:53:22 +0300

    Day 01 solution :)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
+const val Day = 1
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val DayIdentifier = "Day${Day.toString().padStart(2,'0')}"
+    fun part1(input: List<Int>): Int {
+        var prev = -1
+        var count = 0
+        input.forEachIndexed { index, i ->
+            if (prev < i && index != 0) {
+                count++
+            }
+            prev = i
+        }
+        return count
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        val sums = mutableListOf<Int>()
+        input.windowed(3).forEach { sums.add(it.sum()) }
+        return part1(sums)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("${DayIdentifier}_test").map { it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput(DayIdentifier).map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
